1ddd8cb79d2b813c6415f1a86fce9585917fd409
Leonardo Barros <nickolback@gmail.com>
2015-12-19 05:19:27 -0800

    Editando usuários e colocando senha

--- user.pp
+++ user.pp
@@ -1,9 +1,10 @@
-user { 'Elisa':
-  ensure => 'absent',
+user { 'elisa':
+  ensure     => 'present',
+  managehome => true,
+  password   => '8070b0b01d9042fdbc54f095bd2832ef',
 }
-user { 'Jackob':
-  ensure => 'absent',
-}
-user { 'Markus':
-  ensure => 'absent',
+user { 'markus':
+  ensure     => 'present',
+  managehome => true,
+  password   => '23c496d2ee2494b3f380a2bd7380b811',
 }
